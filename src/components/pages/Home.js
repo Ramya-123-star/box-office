e6ea7be012fb9bb3b1365799fd0a62bc5d3a5573
Ramya <ramyabikkodu07@gmail.com>
2021-03-28 14:49:45 +0530

    displayed show results and small search refactor

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,17 @@
 import React , { useState } from 'react'
+import MainPageLayout from './componenets/MainPageLayout';
+import {apiGet} from '../misc/config'
 
 const Home = () => {
   const [input , setInput] = useState('');
+  const [results, setResults] = useState(null);
   const onSearch = () => {
+    apiGet(`search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
     //https://api.tvmaze.com/search/shows?q=woman
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
-      console.log(result);
-    })
 
-  }
+  };
   const onInputChange = ev => {
     setInput(ev.target.value);
     //console.log(ev);
@@ -18,11 +21,31 @@ const Home = () => {
       onSearch();
     }
   }
+  const renderResults = () => {
+    if (results && results.length===0){
+      return <div>No Results </div>
+
+    }
+    if (results && results.length>0){
+      return (
+        <div>
+          
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+
+          ))}
+          </div>
+      );
+
+    }
+    return null;
+  };
   
   return (
     <MainPageLayout>
       <input type="text"  onChane={onInputChange} onKeyDown={onKeyDown} val={input}/>
       <button type="button" onClick={onSearch}>Search</button>
+      {renderResults()}
     </MainPageLayout>
   )
 }
