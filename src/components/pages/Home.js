b7781a7feaaef1d92593d4918b1e0fc9b16665ef
Ramya <ramyabikkodu07@gmail.com>
2021-03-28 13:14:05 +0530

    created search button using the tv maze api

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React , { useState } from 'react'
 
 const Home = () => {
+  const [input , setInput] = useState('');
+  const onSearch = () => {
+    //https://api.tvmaze.com/search/shows?q=woman
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
+      console.log(result);
+    })
+
+  }
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+    //console.log(ev);
+  }
+  const onKeyDown = (ev) => {
+    if (ev.keyCode===13) {
+      onSearch();
+    }
+  }
+  
   return (
     <MainPageLayout>
-      This is Home
+      <input type="text"  onChane={onInputChange} onKeyDown={onKeyDown} val={input}/>
+      <button type="button" onClick={onSearch}>Search</button>
     </MainPageLayout>
   )
 }
